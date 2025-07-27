16ac52bf4555b89a4f6bcd43e8aded7fb9030576
Anirudh Gupta <anirudhgupta113@gmail.com>
2025-07-27 12:51:20 +0530

    added mongodb as db

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const singup = (req,res)=>{
+        
+
+
+}
+
+export const login = (req,res)=>{
+    res.send("this is a login route");
+}
+export const logout = (req,res)=>{
+    res.send("this is a logout route");
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
-const app = express();
+import dotenv from "dotenv"
+import authRoutes from './routes/auth.route.js'
 
-app.listen(5001,()=>{
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT;
+m
+app.use("/api/auth",authRoutes);
+app.get("/",(req,res)=>{
+    res.send("this is home")
+})
+app.listen(PORT,()=>{
     console.log("server chalgya")
 })
