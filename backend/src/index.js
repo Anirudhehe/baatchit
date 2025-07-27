7a9108e019940c22288b05552d5ea24b779e5e99
Anirudh Gupta <anirudhgupta113@gmail.com>
2025-07-28 01:17:12 +0530

    added signup with jwt

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import dotenv from "dotenv"
 import authRoutes from './routes/auth.route.js'
+import {connectDB} from './lib/db.js'
 
 dotenv.config()
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT;
-m
-app.use("/api/auth",authRoutes);
 app.get("/",(req,res)=>{
     res.send("this is home")
 })
+app.use("/api/auth",authRoutes);
+
 app.listen(PORT,()=>{
     console.log("server chalgya")
+    connectDB();
 })
